27c2b5cc5cc1ee48b58d04e67df643407a896523
TJ Stankus <tjstankus@gmail.com>
2020-03-21 14:12:19 -0400

    Timer runs for 5 seconds

--- main.go
+++ main.go
@@ -16,8 +16,8 @@ func main() {
 	// Tickers use a similar mechanism to timers: a
 	// channel that is sent values. Here we'll use the
 	// `select` builtin on the channel to await the
-	// values as they arrive every 500ms.
-	ticker := time.NewTicker(500 * time.Millisecond)
+	// values as they arrive.
+	ticker := time.NewTicker(1 * time.Second)
 	done := make(chan bool)
 
 	go func() {
@@ -34,7 +34,7 @@ func main() {
 	// Tickers can be stopped like timers. Once a ticker
 	// is stopped it won't receive any more values on its
 	// channel. We'll stop ours after 1600ms.
-	time.Sleep(1600 * time.Millisecond)
+	time.Sleep(5 * time.Second)
 	ticker.Stop()
 	done <- true
 	fmt.Println("Ticker stopped")
