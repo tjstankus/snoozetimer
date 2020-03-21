01fc07257cbe9565b336530fa146e2f3082085c0
TJ Stankus <tjstankus@gmail.com>
2020-03-21 14:14:08 -0400

    Mark off a checkbox FTW

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
-// - timer that counts down for 5 seconds
-// - at expire show a notifcation via osascript
+// - [x] timer that counts down for 5 seconds
+// - [ ] at expire show a notifcation via osascript
 //   - search for a more generic library
 //   - maybe abstract notifications into separate package
 // - read response to osascript dialog
