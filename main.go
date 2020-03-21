0c97c685c3bfd744895c09be6b7363cfb2e1e21f
TJ Stankus <tjstankus@gmail.com>
2020-03-21 11:55:23 -0400

    Write out some initial feature ideas
    I have almost no idea what I'm doing in Go. I'm the proverbial dog
    smacking their paws on the keyboard.

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+// - timer that counts down for 5 seconds
+// - at expire show a notifcation via osascript
+//   - search for a more generic library
+//   - maybe abstract notifications into separate package
+// - read response to osascript dialog
+// - snooze that fucker
+
+func main() {
+
+}
